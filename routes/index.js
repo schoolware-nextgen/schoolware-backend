79957f6998486772513362036ff699dfed3bcc76
Maarten Buelens <maarten@mail.mb-server.com>
2024-09-02 15:34:11 +0000

    update redirect

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect("http://frontend.coolify.mb")
+  res.redirect("https://frontend.mb-server.com")
   res.render('index', { title: 'Express' });
 });
 
